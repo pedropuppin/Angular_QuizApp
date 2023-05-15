6881c8e7e2af106703b5c7bef540b863e988e317
pedro puppin <puppin.pedro@gmail.com>
2023-05-15 09:20:57 -0300

    home page edit

--- src/app/features/home/pages/home-page/home-page.component.ts
+++ src/app/features/home/pages/home-page/home-page.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/shared/types/user.model';
 
 @Component({
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
+  constructor(
+    private router: Router
+  ) { }
 
+  user?: User;
+
+  ngOnInit(): void {
+    const userSessionStorage = sessionStorage.getItem('user');
+    if(userSessionStorage) this.user = JSON.parse(userSessionStorage);
+  }
+
+  navigateByUrl(url: string) {
+    this.router.navigateByUrl(url);
+  }
 }
